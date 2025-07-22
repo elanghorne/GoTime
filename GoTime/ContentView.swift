118114069674cfffd3dbb765039db32fe1500cdb
elanghorne <ericlanghorne7@gmail.com>
2025-07-22 10:06:07 -0400

    Initial Commit

---
+++ GoTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoTime
+//
+//  Created by Eric Langhorne on 7/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoTime/GoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTimeApp.swift
+//  GoTime
+//
+//  Created by Eric Langhorne on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct GoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
