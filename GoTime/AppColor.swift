8f1dd6e8fedae7481266b61c76e06a1a9bd6914c
elanghorne <ericlanghorne7@gmail.com>
2025-07-22 10:37:40 -0400

    Create project and set up initial UI layout

---
+++ GoTime/AppColor.swift
@@ -0,0 +1,14 @@
+//
+//  AppColor.swift
+//  GoTime
+//
+//  Created by Eric Langhorne on 7/22/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppColor {
+    static let background = Color(red: 248/255, green: 248/255, blue: 248/255)
+    static let matchIcon = Color(red: 64/255, green: 156/255, blue: 255/255)
+}
--- GoTime/ContentView.swift
+++ GoTime/ContentView.swift
@@ -7,18 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            AppColor.background
+                .ignoresSafeArea() // extends full screen
+
+            VStack {
+                Spacer()
+                Spacer()
+                Text("Rain Alerts")
+                    .bold()
+                    .monospaced()
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+
+                Button(action: {
+                    print("Toggle button pressed")
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 250, height: 250)
+                            .foregroundColor(AppColor.matchIcon)
+
+                        Circle()
+                            .stroke(AppColor.background, lineWidth: 7)
+                            .frame(width: 220, height: 220)
+
+                        Image(systemName: "drop.circle")
+                            .font(.system(size: 75))
+                            .foregroundColor(AppColor.background)
+                    }
+                }
+                .frame(width: 250, height: 250)
+                .background(Color.red.opacity(0.3))
+                .clipShape(Circle())
+                .contentShape(Circle())
+                .padding()
+
+                Spacer()
+
+                HStack {
+                    Button(action: {
+                        print("Info button pressed")
+                    }) {
+                        Image(systemName: "info.circle")
+                            .font(.system(size: 25))
+                    }
+
+                    Spacer()
+
+                    Button(action: {
+                        print("Settings button pressed")
+                    }) {
+                        Image(systemName: "gearshape")
+                            .font(.system(size: 25))
+                    }
+                }
+                .padding(.horizontal, 30)
+                .padding(.bottom, 30)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
